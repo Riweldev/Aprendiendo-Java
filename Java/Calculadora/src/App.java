86ff3c59105e5439b612f14a6240c730f6107397
Joel Nuñez <joelezequieldev@gmail.com>
2026-03-17 23:50:01 -0300

    finalizando calculadora

--- Java/Calculadora/src/App.java
+++ Java/Calculadora/src/App.java
@@ -1,12 +1,14 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
     double primerNumero , segundoNumero;
     double resultado=0;
     String operacion,otraMas;
     Scanner sc = new Scanner(System.in);
     do {
+        boolean operacionValida = true;
         try {
             System.out.println("Ingrese el Primer numero");
             primerNumero = sc.nextDouble();
@@ -18,36 +20,27 @@ public class App {
             operacion = sc.next();
             System.out.println("Ingrese el Segundo numero");
             segundoNumero = sc.nextDouble();
-        } catch (Exception error) {
-            System.out.println("Error: un valor no es un numero");
-            sc.next();
-        }
-        System.out.println("Ingrese el Primer numero");
-        primerNumero = sc.nextDouble();
-        System.out.println("Que division quiere realizar?");
-        System.out.println("1. Suma");
-        System.out.println("2. Resta");
-        System.out.println("3. Multiplicacion");
-        System.out.println("4. Division");
-        operacion = sc.next();
-        System.out.println("Ingrese el Segundo numero");
-        segundoNumero = sc.nextDouble();
-        if (operacion.equalsIgnoreCase("Suma") || operacion.equals("1")) {
-            resultado = primerNumero + segundoNumero;
-            System.out.println("Su resultado es "+ resultado); 
-        }else if (operacion.equalsIgnoreCase("Resta") || operacion.equals("2")) {
-            resultado = primerNumero - segundoNumero;
-            System.out.println("Su resultado es "+ resultado); 
-        }else if (operacion.equalsIgnoreCase("Multiplicacion") || operacion.equals("3")) {
-            resultado = primerNumero * segundoNumero;
-            System.out.println("Su resultado es "+ resultado); 
-        }else if (operacion.equalsIgnoreCase("Division") || operacion.equals("4")) {
-            if (segundoNumero == 0) {
-                System.out.println("No se puede dividir por 0");
-            }else{
-                resultado = primerNumero / segundoNumero;
-                System.out.println("Su resultado es "+ resultado); 
+            if (operacion.equalsIgnoreCase("Suma") || operacion.equals("1")) {
+                resultado = primerNumero + segundoNumero;
+            }else if (operacion.equalsIgnoreCase("Resta") || operacion.equals("2")) {
+                resultado = primerNumero - segundoNumero;
+            }else if (operacion.equalsIgnoreCase("Multiplicacion") || operacion.equals("3")) {
+                resultado = primerNumero * segundoNumero;
+            }else if (operacion.equalsIgnoreCase("Division") || operacion.equals("4")) {
+                if (segundoNumero == 0) {
+                    System.out.println("No se puede dividir por cero.");
+                    operacionValida = false;
+                } else {
+                    resultado = primerNumero / segundoNumero;
+                }
             }
+            if (operacionValida) {
+                System.out.println("Su Resultado es "+ resultado);
+            }
+        } catch (InputMismatchException e) {
+            sc.nextLine();
+            System.out.println(e);
+            System.out.println("No Es posible realizar la operacion");
         }
         System.out.println("Desea hacer otra operacion?");
         otraMas = sc.next();
